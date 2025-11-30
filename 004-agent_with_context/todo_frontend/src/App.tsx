a273e3ec25a043e9f93e85b65ba3de4c45a3adfd
Atsushi Morimoto <74th.tech@gmail.com>
2025-11-30 18:57:46 +0900

    fix compile error

--- 004-agent_with_context/todo_frontend/src/App.tsx
+++ 004-agent_with_context/todo_frontend/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import * as api from "./api/task";
+import type { Task } from "./entity/tasks";
+import { NewTaskForm } from "./views/newTask";
+import { ListTaskView } from "./views/taskList";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App() {
+    const [taskList, setTasks] = useState<Task[]>([]);
+
+    async function reloadTasks() {
+        const tasks = await api.loadTasks();
+        setTasks(tasks);
+    }
+
+    useEffect(() => {
+        reloadTasks();
+    }, []);
+
+    return (
+        <div className="container">
+            <NewTaskForm reloadTasks={reloadTasks} />
+            <ListTaskView taskList={taskList} reloadTasks={reloadTasks} />
+        </div>
+    );
 }
 
 export default App
---
+++ 004-agent_with_context/todo_frontend/src/entity/tasks.ts
@@ -0,0 +1,17 @@
+export type TaskStatus = "todo" | "done";
+
+/**
+ * タスク
+ */
+export interface Task {
+    id?: number;
+    text: string;
+    status?: TaskStatus;
+}
+
+export function createEmptyTask(): Task {
+    return {
+        text: "",
+        status: "todo",
+    };
+}
